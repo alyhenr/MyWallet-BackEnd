cc518d9d34a2002de9cbf432738c9b15221f557a
Alysson Henrique <alyssonhra99@gmail.com>
2023-07-11 11:10:03 -0300

    fix: .env config for deploy

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import cors from "cors";
+import dotenv from "dotenv";
 
 import routes from "./routes/index.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 app.use(routes);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`)
+    console.log(`Servidor rodando na porta ${port}`);
 })
